520541c17aa4f62eec1712d552a6b481eaa72171
Andee Warren <119145298+andeewarren@users.noreply.github.com>
2023-04-04 12:20:38 -0400

    updated styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (  
         <div className='header'>
-            <h1>Personal Library</h1>
+            <h1>My Bookshelf</h1>
         </div>
     );
 }
